b9f063990fd34b926141921d42c7423492bcee0c
csarq <catherinesarquis@gmail.com>
2025-06-07 15:46:45 +0100

    add date display and set basic html selectors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert("Heya!");
+const singaporeElement = document.querySelector("#singapore");
+const singaporeDateElement = singaporeElement.querySelector(".date-display");
+const singaporeTimeElement = singaporeElement.querySelector(".time-display");
+singaporeDateElement.innerHTML = "7th of June, 2025";
+singaporeTimeElement.innerHTML = "14:00:46";
