d92ad6b76e7182698e4f58564f64c409d9c31d19
csarq <catherinesarquis@gmail.com>
2025-06-07 16:18:43 +0100

    js function to update times and dates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-const singaporeElement = document.querySelector("#singapore");
-const singaporeDateElement = singaporeElement.querySelector(".date-display");
-const singaporeTimeElement = singaporeElement.querySelector(".time-display");
-singaporeDateElement.innerHTML = "7th of June, 2025";
-singaporeTimeElement.innerHTML = "14:00:46";
+function updateTime() {
+  //singapore
+  const singaporeElement = document.querySelector("#singapore");
+  const singaporeDateElement = singaporeElement.querySelector(".date-display");
+  const singaporeTimeElement = singaporeElement.querySelector(".time-display");
+  singaporeDateElement.innerHTML = moment()
+    .tz("Asia/Singapore")
+    .format("Do [of] MMMM[,] YYYY");
+  singaporeTimeElement.innerHTML = moment()
+    .tz("Asia/Singapore")
+    .format("HH[:]mm[:]ss");
+
+  //london
+  const londonElement = document.querySelector("#london");
+  const londonDateElement = londonElement.querySelector(".date-display");
+  const londonTimeElement = londonElement.querySelector(".time-display");
+  londonDateElement.innerHTML = moment()
+    .tz("Europe/london")
+    .format("Do [of] MMMM[,] YYYY");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/london")
+    .format("HH[:]mm[:]ss");
+
+  //new york
+  const newyorkElement = document.querySelector("#new-york");
+  const newyorkDateElement = newyorkElement.querySelector(".date-display");
+  const newyorkTimeElement = newyorkElement.querySelector(".time-display");
+  newyorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("Do [of] MMMM[,] YYYY");
+  newyorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("HH[:]mm[:]ss");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
